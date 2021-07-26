8660c296274bcaf6a50b6a389fc9fdc372c4bc99
bujosa <davidbujosa@gmail.com>
2021-07-26 03:31:52 -0400

    added main

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+
+	"github.com/Venus-Golang/controllers"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+	c := controllers.NewController(tpl)
+	http.HandleFunc("/", c.Index)
+	http.HandleFunc("/bar", c.Bar)
+	http.HandleFunc("/signup", c.SignUp)
+	http.HandleFunc("/login", c.Login)
+	http.HandleFunc("/logout", c.Logout)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
