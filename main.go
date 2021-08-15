852a075b6b925cb3a3e04d448f65aeb545b88cff
bujosa <davidbujosa@gmail.com>
2021-08-14 22:58:51 -0400

    Fixed main

--- main.go
+++ main.go
@@ -10,10 +10,15 @@ import (
 
 func main() {
 	r := httprouter.New()
+	
 	uc := controllers.NewUserController(getSession())
+
 	r.GET("/user/:id", uc.GetUser)
+
 	r.POST("/user", uc.CreateUser)
+
 	r.DELETE("/user/:id", uc.DeleteUser)
+
 	http.ListenAndServe("localhost:8080", r)
 }
 
